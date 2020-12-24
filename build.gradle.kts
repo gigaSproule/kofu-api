9afdcdc89388f077fbe729ac685978b8278c7d72
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2020-12-24 00:44:55 +0000

    Changed to JDK 14

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,14 +48,14 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_15
-    targetCompatibility = JavaVersion.VERSION_15
+    sourceCompatibility = JavaVersion.VERSION_14
+    targetCompatibility = JavaVersion.VERSION_14
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "15"
+        jvmTarget = "14"
         incremental = true
     }
 }
